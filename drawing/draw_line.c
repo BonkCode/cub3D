7d5bacd785fa74f00289607d20b7065cba47e8db
BonkCode <62333148+BonkCode@users.noreply.github.com>
2020-08-25 22:21:56 +0300

    erased some stuff, added gap solve comment
    comment on how to solve gap problem

--- drawing/draw_line.c
+++ drawing/draw_line.c
@@ -41,12 +41,7 @@ void	draw_line(t_data *img, t_vector2 start, t_vector2 end, int thickness, int c
 		current_b = b;
 		while (pos.x < end.x)
 		{
-			pos.y = k * pos.x + current_b;
-			if (local_abs(pos.x - (int)pos.x) + local_abs(pos.y - (int)pos.y) > LINE_THRESHOLD)
-			{
-				my_mlx_pixel_put(img, (int)(pos.x + 1), (int)pos.y, color);
-				my_mlx_pixel_put(img, (int)(pos.x - 1), (int)pos.y, color);
-			}
+			pos.y = k * pos.x + current_b; // store prev_pos.y, check if the are far (y1 - y2 > 1) and draw pixels in between in a while loop 
 			my_mlx_pixel_put(img, (int)pos.x, (int)pos.y, color);
 			++pos.x;
 		}
